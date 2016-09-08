cb4ca9c6c026547beb9c8bd32102faa026f1d806
jacob_smith <jacob.smith6@griffithuni.edu.au>
2016-09-08 06:40:00 +0000

    database hooked up and working

--- routes.js
+++ routes.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var router = express.Router();
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.defaults.poolSize = 10;
+
 
 
 /* -------example for API routing below ---------
@@ -29,6 +34,32 @@ router.route('/users/:user_id')
 });
 
 */
+
+
+//db test route
+
+router.route('/testdb')
+
+.all(function(req, res, next) {
+  // do stuff
+  next();
+})
+
+.get(function(req, res, next) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if(err) { return console.error('error fetching client from pool', err); }
+    client.query('SELECT * FROM customer', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { res.json(result.rows); }
+    });
+  });
+})
+
+
+
 
 
 //Front End Route - handles all angular requests--------------------------------
